52006f7e24989ff30a336e6099522d8913c2a3de
mac <mac@MacBook-Pro.local>
2020-08-01 20:20:26 +0300

    Add create employee function to employee api
    Call the backend api to create a new employee

--- react-app/src/api/EmployeeApi.tsx
+++ react-app/src/api/EmployeeApi.tsx
@@ -1,10 +1,20 @@
 import axios from 'axios';
+import EmployeeProps from '../interfaces/employee';
 
 const baseUrl = 'http://localhost:8080';
 
 export const fetchEmployees = async () => {
     try {
         return await axios.get(`${baseUrl}/employees`);;
+    } catch (error) {
+        console.log("error : ", error);
+        throw error;
+    }
+}
+
+export const createEmployee = async (employee: EmployeeProps) => {
+    try {
+        return axios.post(`${baseUrl}/employees`, employee);
     } catch (error) {
         console.log("error", error);
         throw error;
