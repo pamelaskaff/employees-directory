a36cde395e6b1b97c7062541b5dec37c1fb3360a
mac <mac@MacBook-Pro.local>
2020-08-01 14:24:43 +0300

    Render Employees list when going to /home

--- react-app/src/components/Header/Header.tsx
+++ react-app/src/components/Header/Header.tsx
@@ -3,6 +3,8 @@ import Nav from 'react-bootstrap/Nav';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from '../Home/Home'
+import EmployeeList from '../EmployeesList/EmployeesList';
+
 
 
 const Header = () => {
@@ -13,9 +15,8 @@ const Header = () => {
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
                 </Nav.Item>
             </Nav>
-            <Route path="/home" component={Home} />
+            <Route path="/home" component={EmployeeList} />
         </Router>
-
     )
 }
 
