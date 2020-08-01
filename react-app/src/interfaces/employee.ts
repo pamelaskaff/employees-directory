a5e8f5538dd327d60daab4eb983d20ee459bf088
mac <mac@MacBook-Pro.local>
2020-08-01 13:58:03 +0300

    Add Employee interface

---
+++ react-app/src/interfaces/employee.ts
@@ -0,0 +1,13 @@
+interface EmployeeProps {
+    id: string,
+    name: string,
+    age : number,
+    location: string,
+    image?: string,
+    role: string,
+    salary?: number,
+    email: string,
+    phone: string
+  }
+
+  export default EmployeeProps;
