8d8424063b340a481915f4ccb1581e1118dd968d
mac <mac@MacBook-Pro.local>
2020-08-01 19:37:15 +0300

    Add create employee function to nodejs employee controller
    Add write file helper method

--- nodejs-app/Models/Employee/EmployeeController.js
+++ nodejs-app/Models/Employee/EmployeeController.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const employees = require('../../database/employees.json')
 const dataPath = path.join(__dirname, '../../database/employees.json');
 const fs = require('fs');
 
@@ -13,6 +14,25 @@ const readFile = (callback, returnJson = false, filePath = dataPath, encoding = 
     });
 };
 
+
+const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+    fs.writeFile(filePath, fileData, encoding, (err, data) => {
+        if (err) {
+            throw err;
+        }
+        callback();
+    });
+};
+
+//Check if required parameters are passed
+function checkParams(req) {
+    console.log("in checkparams", req.body)
+    if (req.body.name == '' || req.body.location == '' || req.body.age == null || req.body.image == '' || req.body.role == '' || req.body.email == '' || req.body.phone == '')
+        return false;
+    else
+        return true;
+}
+
 //Get all employees
 exports.getAllEmployees = function (req, res) {
     try {
@@ -23,3 +43,30 @@ exports.getAllEmployees = function (req, res) {
         res.status(500).send("Internal server error");
     }
 }
+
+//Create a new Employee
+exports.createEmployee = function (req, res) {
+    try {
+        if (!checkParams(req)) {
+            console.log("MISSING INFOOO!")
+            res.status(422).send("Missing information");
+        }
+        readFile(dataPath => {
+            const newEmployeeId = Object.keys(dataPath).length + 1;
+            req.body.id = newEmployeeId.toString();
+            employees.push(req.body);
+
+            // add the new employee id
+            dataPath[newEmployeeId.toString()] = req.body;
+
+            // add employee to the json database file
+            writeFile(JSON.stringify(employees), () => {
+                res.status(200).send("New Employee added");
+                console.log("res", res.body);
+            });
+        }, true);
+    } catch{
+        console.log("There was a problem creating an employee", err);
+        res.status(500).send("There was a problem adding the employee");
+    }
+};
